4bcb8f0fc5a161a54b5ad5cea3146e58b2b9137d
chengmiao <mandycheng0908@foxmail.com>
2024-06-03 14:11:25 +0800

    feat: transition test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,24 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['@/assets/css/app.scss'],
   // 全局注册组件 
-  components:{
+  components: {
     global: true,
     dirs: [
       '~/components',
     ],
   },
-  imports:{
+  imports: {
     // 配置全子目录支持自动导入
-    dirs:[
+    dirs: [
       "utils/**",
       "composable/**"
     ]
+  },
+  app: {
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in'
+    },
+    layoutTransition: { name: 'rotate', mode: 'out-in' },
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-
-    <NuxtWelcome />
-
+  <NuxtWelcome />
+  <div v-for="(page, index) in pageLinks" :key="index">
+    <NuxtLink :to="page.url">{{ page.text }}</NuxtLink>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const pageLinks = [
+  {
+    url: '/transition/page',
+    text: '4.9.1 页面过渡动画'
+  }
+]
+</script>
 
-<style scoped lang='scss'></style>
+<style scoped lang="scss"></style>
